7be03ae6d1f5da6e0c8155c1d37a50fedb5d4db5
Lluis Soberats <lsoberats@nigul.cide.es>
2026-06-07 15:39:36 +0200

    Create main application file for Spring Boot

---
+++ src/main/kotlin/com/kitdigital/Application.kt
@@ -0,0 +1,11 @@
+package com.kitdigital
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KitDigitalApplication
+
+fun main(args: Array<String>) {
+    runApplication<KitDigitalApplication>(*args)
+}
